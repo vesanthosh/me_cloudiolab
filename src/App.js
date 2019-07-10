a0685d893e1cfcbea51d1dc668875b3372cf4c2c
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-07-10 21:23:12 +0200

    - App.js adjustments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import AboutMe from './components/AboutMe';
+import Resume from './components/Resume';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={AboutMe} />
+          <Route exact path="/resume" component={Resume} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
