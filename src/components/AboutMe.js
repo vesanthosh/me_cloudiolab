939369459d19c4a3c8970aee82a2e593a5785059
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-07-15 18:12:21 +0200

    - small corrections

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -23,18 +23,18 @@ class AboutMe extends Component {
                         </table>
                         <hr />
                         <div className="text-right">
-                            <p>Greetings from India.</p>
+                            <p><i>Greetings from India.</i></p>
                             <p>I moved to Germany for master studies.</p>
                             <p>I now live in Heilbronn, Baden-Württemberg in Deutschland.</p>
                             <p>I read tech journals, blogs, small story books to learn new languages and listen radio a lot.</p>
                             <p>I watch a lot movies and I used to organize a weekly Cricket match with friends in Heilbronn.</p>
                             <p>I obsess on building my own concept <a href="https://dothings.cloudiolab.com" target="_blank" rel="noopener noreferrer">full stack website</a> when I'm not doing my <a href="https://www.linkedin.com/in/santhoshvenkatesan" target="_blank" rel="noopener noreferrer">professional work</a>.</p>
-                            <p>I run my own cluster of 3 servers where I deploy all my service containers to the kubernetes cluster.</p>
+                            <p>I run my own cluster of 3 servers where I deploy all my service containers to the swarm/kubernetes cluster.</p>
                             <p>I also run <a href="https://docker.cloudiolab.com" target="_blank" rel="noopener noreferrer">private docker registry</a> to maintain a production grade service containers of mine.</p>
                             <p>I have a motivating brother, supporting family and a hyperactive Labrador Retriever.</p>
                             <p>I write <a href="https://github.com/vesanthosh" target="_blank" rel="noopener noreferrer">code</a> and (used to) convert hot coffee into awesome code.</p>
                             <p>I was doing freelancing work for clients in need of adjustments to their deployed websites for about a year in Bangalore.</p>
-                            <p>I worked with a startup team of 8 people to make design templates and implement client and service side business logic. </p>
+                            <p>I worked with a startup team of 8 people to make design templates and implement client and server side logic. </p>
                             <p>I am not anymore, but still they're doing good.</p>
                             <p>I worked on the government funded project sponsored by Tamil Nadu State Council for Science & Technology.</p>
                             <p>I did a great contribution towards protecting the environment by developing a centralized EMF monitoring system for the State Government.</p>
